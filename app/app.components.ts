a2eee4cc79daf55de64317d7197e711f403b9614
luis <ferrerluis97@gmail.com>
2016-07-03 16:01:46 -0400

    Angular 2 structure created

---
+++ app/app.components.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by luis on 7/1/16.
+ */
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'main-app',
+    template: '<h1>First Angular App</h1>'
+})
+
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by luis on 7/2/16.
+ */
+
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
