3aa4eb999af610d7335fcba66c62429442e58f77
luis <ferrerluis97@gmail.com>
2016-07-03 16:05:31 -0400

    Fixed typo

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,6 @@
 
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.components';
 
 bootstrap(AppComponent);
