3abb286fe64175a8cfa48624f684a68ed33c36db
Luis Ferrer-Labarca <ferrerluis97@gmail.com>
2016-07-22 02:17:00 +0000

    Changed 'response' on callback function to 'projects'

--- app/components/coding/codingController.js
+++ app/components/coding/codingController.js
@@ -6,8 +6,8 @@ app.controller('codingController', function($scope, $http, $log) {
 
     $scope.page = new Page('coding');
 
-    Project.all($http, function (response) {
-        $scope.page.projects = response.exclude(function (value) {
+    Project.all($http, function (projects) {
+        $scope.page.projects = projects.exclude(function (value) {
             return value.url === "https://github.com/ferrerluis/blog";
         });
     });
